0286a16fea898e70a2a8e7cc71dd06d724cb6273
SuzanaNeto2022 <suzananeto22022022@gmail.com>
2024-06-22 22:24:13 +0100

    Adicionei o arquivo arvore

---
+++ Arvorebinaria.py
@@ -0,0 +1,271 @@
+import tkinter.messagebox
+import customtkinter 
+import tkinter
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+
+class No:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return str(self.data)
+
+class BinaryTree:
+    def __init__(self, data=None, node=None):
+        self.root = None  # Inicializa a raiz como None por padrão
+        if node:
+            self.root = node
+        elif data:
+            self.root = No(data)
+
+    def em_ordem_erd(self, node=None):
+        if self.root is None:
+            return []
+        if node is None:
+            node = self.root
+        percurso = []
+        if node.left:
+            percurso += self.em_ordem_erd(node.left)
+        percurso.append(node.data)
+        if node.right:
+            percurso += self.em_ordem_erd(node.right)
+        return percurso
+
+    def pos_ordem_edr(self, node=None):
+        if self.root is None:
+            return []
+        if node is None:
+            node = self.root
+        percurso = []
+        if node.left:
+            percurso += self.pos_ordem_edr(node.left)
+        if node.right:
+            percurso += self.pos_ordem_edr(node.right)
+        percurso.append(node.data)
+        return percurso
+
+    def pre_ordem(self, node=None):
+        if self.root is None:
+            return []
+        if node is None:
+            node = self.root
+        percurso = [node.data]
+        if node.left:
+            percurso += self.pre_ordem(node.left)
+        if node.right:
+            percurso += self.pre_ordem(node.right)
+        return percurso
+
+    def altura(self, node=None):
+        if self.root is None:
+            return -1
+        if node is None:
+            node = self.root
+        altura_esquerda = 0
+        altura_direita = 0
+        if node.left:
+            altura_esquerda = self.altura(node.left)
+        if node.right:
+            altura_direita = self.altura(node.right)
+        return max(altura_esquerda, altura_direita) + 1
+
+    def imprimir_arvore(self, node=None, level=0, prefix="Root"):
+        if self.root is None:
+            return
+        if node is None:
+            node = self.root
+        print(" " * (level * 4) + prefix + ": " + str(node.data))
+        if node.left:
+            self.imprimir_arvore(node.left, level + 1, "L---")
+        if node.right:
+            self.imprimir_arvore(node.right, level + 1, "R---")
+
+class ArvoreBinariaBusca(BinaryTree):
+    def inserir(self, valor):
+        parent = None
+        x = self.root
+        node = No(valor)
+        while x:
+            parent = x
+            if valor < x.data:
+                x = x.left
+            else:
+                x = x.right
+        if parent is None:
+            self.root = node
+        elif valor < parent.data:
+            parent.left = node
+        else:
+            parent.right = node
+    
+    
+    ##########################################################
+    def remover(self, valor):
+        return self._remover_recursivo(self.root, valor)
+
+    def _remover_recursivo(self, node, valor):
+        if node is None:
+            return None
+
+        if valor < node.data:
+            node.left = self._remover_recursivo(node.left, valor)
+        elif valor > node.data:
+            node.right = self._remover_recursivo(node.right, valor)
+        else:
+            # Caso 1: Nó sem filhos
+            if node.left is None and node.right is None:
+                return None
+            # Caso 2: Nó com um filho
+            if node.left is None:
+                return node.right
+            if node.right is None:
+                return node.left
+            # Caso 3: Nó com dois filhos
+            menor_direita = self._encontrar_menor(node.right)
+            node.data = menor_direita.data
+            node.right = self._remover_recursivo(node.right, menor_direita.data)
+
+        return node
+
+    def _encontrar_menor(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
+    
+
+####################################################3  CTkCanvas
+
+class TreeVisualizer(customtkinter.CTkScrollableFrame):
+    def __init__(self, master=None, tree=None):
+        super().__init__(master)
+        self.master = master
+        self.tree = tree
+        self.master.title("Visualização da Árvore Binária")
+        self.canvas_width = 800
+        self.canvas_height = 650
+        self.create_widgets()
+        
+    def create_widgets(self):
+        self.canvas = customtkinter.CTkCanvas(self, width=self.canvas_width, height=self.canvas_height, bg="white")
+        self.canvas.pack(expand=True, fill=customtkinter.BOTH)
+
+        # Frame para entrada de dados
+        input_frame = customtkinter.CTkCanvas(self)
+        input_frame.pack()
+
+        self.entry_valor = customtkinter.CTkEntry(input_frame,placeholder_text='Insira um valor na árvore',  font=("", 14))
+        self.entry_valor.pack(side=customtkinter.LEFT,padx=2)       
+        btn_inserir = customtkinter.CTkButton(input_frame, text="Inserir", command=self.inserir_valor)
+        btn_inserir.pack(side=customtkinter.LEFT,padx=10)
+        ## botao remover nó
+        btn_remover= customtkinter.CTkButton(input_frame, text="remover", command=self.remover_valor)
+        btn_remover.pack(side=customtkinter.LEFT, padx=10)
+
+        
+        # Frame para exibição dos percursos
+        percurso_frame = customtkinter.CTkFrame(self)
+        percurso_frame.pack(pady=10)
+
+        self.txt_em_ordem = customtkinter.CTkTextbox(percurso_frame, width=400, height=300, wrap=customtkinter.WORD)
+        self.txt_em_ordem.pack(side=customtkinter.LEFT, padx=10)
+        
+        
+        self.txt_pre_ordem = customtkinter.CTkTextbox(percurso_frame, width=400, height=300, wrap=customtkinter.WORD)
+        self.txt_pre_ordem.pack(side=customtkinter.LEFT, padx=10)
+        
+        self.txt_pos_ordem = customtkinter.CTkTextbox(percurso_frame, width=400, height=300, wrap=customtkinter.WORD)
+        self.txt_pos_ordem.pack(side=customtkinter.LEFT, padx=10)
+
+
+        
+        self.draw_tree()
+
+    def draw_tree(self):
+        self.canvas.delete("all")  # Limpa o canvas antes de redesenhar
+        if self.tree.root:
+            self.draw_node(self.tree.root, self.canvas_width // 2, 50, self.canvas_width // 4)
+        
+        # Atualiza as áreas de texto dos percursos
+        em_ordem = self.tree.em_ordem_erd()
+        self.txt_em_ordem.delete("1.0", customtkinter.END)
+        self.txt_em_ordem.insert(customtkinter.END, "Em Ordem (ERD):\n" + ", ".join(map(str, em_ordem)))
+
+        pre_ordem = self.tree.pre_ordem()
+        self.txt_pre_ordem.delete("1.0", customtkinter.END)
+        self.txt_pre_ordem.insert(customtkinter.END, "Pré-Ordem:\n" + ", ".join(map(str, pre_ordem)))
+        
+        pos_ordem = self.tree.pos_ordem_edr()
+        self.txt_pos_ordem.delete("1.0", customtkinter.END)
+        self.txt_pos_ordem.insert(customtkinter.END, "Pós-Ordem (EDR):\n" + ", ".join(map(str, pos_ordem)))
+
+        
+    def draw_node(self, node, x, y, h_space):
+        if node.left:
+            x_left = x - h_space
+            y_left = y + 100
+            self.canvas.create_line(x, y, x_left, y_left, width=2)
+            self.draw_node(node.left, x_left, y_left, h_space // 2)
+        
+        if node.right:
+            x_right = x + h_space
+            y_right = y + 100
+            self.canvas.create_line(x, y, x_right, y_right, width=2)
+            self.draw_node(node.right, x_right, y_right, h_space // 2)
+
+             # Draw node
+        radius = 20
+        self.canvas.create_oval(x - radius, y - radius, x + radius, y + radius, fill="lightblue")
+        self.canvas.create_text(x, y, text=str(node.data), font=("Arial", 12))
+
+    def inserir_valor(self):
+        valor = self.entry_valor.get()
+        if valor == '':
+            print('Adiciona um elemento na lista')
+            tkinter.messagebox.showwarning("Mensagem de Erro", "Adiciona um elemento na árvore")
+            
+
+        elif valor.isdigit():
+            valor = int(valor)
+            self.tree.inserir(valor)
+            self.draw_tree()
+            self.entry_valor.delete(0,"end")
+        else:
+            print("Valor inválido. Insira um número inteiro.")
+    # remover nó
+    def remover_valor(self):
+        valor = self.entry_valor.get()  
+        if valor == '':
+            print('Adiciona um elemento na lista')
+            tkinter.messagebox.showwarning("Mensagem de Erro", "Adiciona um elemento na caixa de texto")
+            
+        elif valor.isdigit():
+            valor = int(valor)
+            self.tree.remover(valor)
+            self.draw_tree()
+            self.entry_valor.delete(0,"end")
+
+
+        else:
+                print(f"O valor {valor} não foi encontrado na árvore.")
+        #else:
+        #    print("Valor inválido. Insira um número inteiro.")
+    
+
+        
+        
+      
+def main():
+    arvore = ArvoreBinariaBusca()
+    root = customtkinter.CTk()
+    app = TreeVisualizer(master=root, tree=arvore)
+    app.pack(expand=True, fill=customtkinter.BOTH)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
